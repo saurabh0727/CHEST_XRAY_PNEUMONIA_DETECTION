ddf0f220f3fe8cd2bba2db267d4b316b6728193b
saurabh0727 <saurabhbasediya3@gamil.com>
2024-03-20 12:20:11 +0530

    Added Link to Github & Contact-us

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import Logo from './images/logo.png'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const emailAddress = 'saurabhbasediya3@gmail.com';
   return (
     <div className="main-navbar-div">
-        <div className="navbar-left-div">
-            <img src={Logo} alt="logo"/>
-        </div>
+      <div className="navbar-left-div">
+        <img src={Logo} alt="logo" />
+      </div>
 
-        <div className='navbar-right-div'>
-            <ul>
-                <li>About us</li>
-                <li>Contact us</li>
-                <li>Github Access</li>
-            </ul>
-        </div>
+      <div className='navbar-right-div'>
+        <ul>
+          <li>About us</li>
+          <li><a href={`mailto:${emailAddress}`} style={{color:'inherit',textDecoration:'none'}}>Contact Us</a>
+          </li>
+          <li><Link to={'https://github.com/saurabh0727/CHEST_XRAY_PNEUMONIA_DETECTION'} target='_blank' style={{color:'inherit',textDecoration:'none'}}>Github Access</Link></li>
+        </ul>
+      </div>
     </div>
   )
 }
